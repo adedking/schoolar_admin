9f23880e591073326485d6f79f28de3956df7589
Ade <adedokun@ropay.ng>
2023-05-27 17:17:40 +0100

    Auth pages completed update

--- src/pages/auth/forgot-password.js
+++ src/pages/auth/forgot-password.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import AuthLayout from '../../components/layouts/authentication';
-import { Button, Checkbox, Form, Stack, TextInput } from '@carbon/react';
+import { Button, Form, Stack, TextInput } from '@carbon/react';
 import { ArrowRight } from '@carbon/icons-react';
 import { useNavigate } from 'react-router-dom';
 
