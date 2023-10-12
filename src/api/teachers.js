0f83891f2bbe4f8c681b178c025648791616e632
Ade <adedokun@ropay.ng>
2023-10-12 23:18:25 +0100

    Class update

--- src/api/teachers.js
+++ src/api/teachers.js
@@ -2,7 +2,7 @@ import { Axios } from './axios';
 
 const getTeachers = async (payload) => {
     let search = payload.search ? '&search=' + payload.search : '';
-    let filter = payload.statusFilter && payload.statusFilter != -1 ? '&filter=' + payload.statusFilter : '';
+    let filter = payload.statusFilter && payload.statusFilter !== -1 ? '&filter=' + payload.statusFilter : '';
     const { data } = await Axios.get(`/school-teachers?limit=${payload.limit}&page=${payload.page}${search}${filter}`,
     {
       timeout: 0
