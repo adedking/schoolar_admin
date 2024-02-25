6019d107823dd73a46019aae85f31358aaa7ebed
Ade <adedokun@ropay.ng>
2024-02-25 22:09:46 +0100

    Student update

--- src/pages/students/sub-components/view-student/attendance.js
+++ src/pages/students/sub-components/view-student/attendance.js
@@ -1,12 +1,7 @@
 import React from 'react';
-// import { useGetStudent } from '../../../../redux/students/hook';
-// import { useParams } from 'react-router-dom';
 
 const StudentAttendance = () => {
 
-    // const {id} = useParams();
-    // const { data: student } = useGetStudent(id);
-    // console.log(student)
     return (
         <div className='flex flex-col gap-4'>
             <div className='text-[20px] py-4'>
